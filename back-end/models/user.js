c8bae1bf7e89f05e7f69545edde0aa91ab1dbca4
bahaa eddine tayab <bahae.study@gmail.com>
2024-11-14 23:08:33 -0500

    update

---
+++ back-end/models/user.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+var Schema = mongoose.Schema;
+
+var user = new Schema({
+    name:{
+        type:String,
+        required:[true,'name is required']
+
+    },
+    username:{
+        type:String,
+        required:[true,'username is required'],
+        unique:true
+        
+    }
+    ,
+    email:{
+        type:String,
+        required:[true,'email is required']
+    }
+    ,
+    password:{
+        type:String,
+        required:[true,'password is required'],
+        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, "Password invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    },
+    role:{
+        type:String,
+        required:[true,'role is required'],
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    phone_number:{
+        type:String,
+        required:[true,'phone number is required']
+    },
+    homeadresse:{
+        type:String,
+        maxlength: 200
+    },
+    billingadresse:{
+        type:String,
+        maxlength:200
+    },
+    image:{
+        type:String,
+        default:""
+    }
+
+    }
+    , {timestamps : true});
+
+    user.pre('save',async function (next){
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+      next();
+    })
+
+
+const User = mongoose.model('User', user );
+module.exports = User;
